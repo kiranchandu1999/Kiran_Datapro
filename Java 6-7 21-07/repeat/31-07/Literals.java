c25d159cc53bc14a2a2bdddc6e02c36ab85b38e7
kiranchandu1999 <kiransaragadam247@gmail.com>
2025-07-31 19:08:30 +0530

    31/07

---
+++ Java 6-7 21-07/repeat/31-07/Literals.java
@@ -0,0 +1,19 @@
+class Literals {
+    public static void main(String[] args) {
+        // variables
+        int a = 50;
+        long b = 50L;
+
+        float c = 20.7f; // 6 digits
+        double d = 56.86;
+
+        char ch = 'x';
+        boolean bool = true;
+        String s1 = "Datapro";
+
+        System.out.println(a);
+
+        // unicode system
+        System.out.println("\u0041"); // A
+    }
+}
